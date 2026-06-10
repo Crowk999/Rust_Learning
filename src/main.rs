65b66f061fd6c69774d88a25026ebca86a1cff35
Adhrit <adhritsigdel@gmail.com>
2026-06-10 20:35:06 +0545

    just starting the journey

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main(){
-    println!("Hello World");
+    let pair = ('A', 65);
+    let _x:i8=11;
+
+    println!("{}",pair.0); // Accessing first element
+    println!("{}",pair.1); // Accessing second element
+
+     // Destructuring a pair.
+    let (letter, _number) = pair;
+    println!("{}",letter);
+    println!("{}",pair.0);
 }
