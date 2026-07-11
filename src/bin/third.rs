8b62319e90ff5dd9dbca32dd25c01c014df2c8ac
Adhrit <adhritsigdel@gmail.com>
2026-07-11 23:41:41 +0545

    _

--- src/bin/third.rs
+++ src/bin/third.rs
@@ -1,3 +1,4 @@
+
 fn main(){
     let mut x:i8=2;
     println!("{}",x);
@@ -12,4 +13,15 @@ fn main(){
         println!("The while loop");
         x += 1;
     }
+     struct User{
+        name:String,
+        age:u8,
+    }   
+
+    let user = User{
+        name:"Adhrit".to_string(),
+        age:16,
+    };
+
+    println!("The Struct Name:{} and age:{}",user.name,user.age);
 }
