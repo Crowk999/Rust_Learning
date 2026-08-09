09f7ebabc128eff862cdd17dcc38836019f14f7b
Adhrit <adhritsigdel@gmail.com>
2026-08-09 09:34:37 +0545

    arary and vec

--- src/bin/array_listall.rs
+++ src/bin/array_listall.rs
@@ -1,5 +1,17 @@
+use std::vec;
+
 fn main(){
     let arr:[i32;5]=[1,2,3,4,5];
-    let _s = &arr[1..2];
-    print!("{:?}",arr.iter().position(|&x| x==3));
+    let s = &arr[1..2];
+    println!("{:?}",arr.iter().position(|&x| x==4));
+    for x in s{
+        println!("Of the s:-{}",x);
+    }
+    println!("arr[2]={}",arr[2]);
+    println!("{:?}",s);
+    let  v: Vec<i16>=vec![11,44,333,22];
+    for x in v{
+    println!("v={}",x);
+    }
+    
 }
